cc16a842b6681efcb9e471bfab8fefa01c34da4f
stark501 <mkudlacej@gmail.com>
2021-10-05 15:58:04 +0200

    Comparator.cz update

--- Open-Lab-03.03/Comparator.cs
+++ Open-Lab-03.03/Comparator.cs
@@ -6,7 +6,10 @@ namespace Open_Lab_03._03
     {
         public bool CompareCharactersCount(string str1, string str2)
         {
-            throw new NotImplementedException();
+
+            bool majurovnakudlzku = str1.Length == str2.Length;
+            return majurovnakudlzku;
+
         }
     }
 }
